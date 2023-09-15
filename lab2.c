4c422256b63f623afeba05348d013362ec414887
tribui1912 <56878689+tribui1912@users.noreply.github.com>
2023-09-15 12:51:56 -0700

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -1,12 +1,15 @@
-#include <avr/io.h>
-#include <util/delay.h>
-
 #define F_CPU 16000000UL //CPU clock 16MHz
 #define BAUD 9600 //define baud
 #define BAUDRATE ((F_CPU)/(BAUD*16UL)-1) //set baud rate value for UBRR
 
+#include <avr/io.h>
+#include <util/delay.h>
+
+
 void usart_ini(void)
 {
+	//clear usart baud rate 
+	UBRR0H &= 0;
 	//set baud rate
 	UBRR0H = (BAUDRATE >> 8); //shift the register right by 8 bit, because the high 8 bit is reserved
 	UBRR0L = BAUDRATE;
@@ -24,22 +27,23 @@ void usart_transmit(unsigned char data)
 	UDR0 = data;
 }
 
+
 void sendstr(const char* str)
 {
 	while(*str)
 	{
 		usart_transmit(*str++);
+		_delay_ms(500);
 	}
 }
 
 int main(void)
 {
 	usart_ini();
-    while (1) 
-    {
-		sendstr("828135536");
-		_delay_ms(500);
-    }
+	while (1)
+	{
+		sendstr("828135536\r\n"); // \r\n for end of line
+
+	}
 	return 0;
 }
-
